59f519ee98e2286be764c7ac70a32724bfc7f058
for_code <83190908+forcodeufrj@users.noreply.github.com>
2024-08-04 16:49:07 -0300

    Update forcode.sql
    Upando a tabela atualizada com alguns dados

--- MySQL/forcode.sql
+++ MySQL/forcode.sql
@@ -1,169 +1,159 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: localhost    Database: for_code
--- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+-- Criando nosso banco de dados
+DROP DATABASE IF EXISTS for_code;
+CREATE DATABASE for_code;
+USE for_code;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Criando a 1ª tabela
+DROP TABLE IF EXISTS apadrinhamento;
+CREATE TABLE apadrinhamento (
+  membro varchar(50) NOT NULL,
+  PRIMARY KEY(membro),
+  diretoria_membro text NOT NULL,
+  padrinho text NOT NULL,
+  diretoria_padrinho text NOT NULL
+);
 
---
--- Table structure for table `apadrinhamento`
---
-
-DROP TABLE IF EXISTS `apadrinhamento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `apadrinhamento` (
-  `membro` text NOT NULL,
-  `diretoria_membro` text NOT NULL,
-  `padrinho` text NOT NULL,
-  `diretoria_padrinho` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `apadrinhamento`
---
-
-LOCK TABLES `apadrinhamento` WRITE;
-/*!40000 ALTER TABLE `apadrinhamento` DISABLE KEYS */;
-INSERT INTO `apadrinhamento` VALUES ('Joao','PRO','Mariana','PRO'),('Rodrigo','MKT','Anna','MKT'),('Bianca','PRE','Eduarda','PRE'),('Renan','PRE','Eduarda','PRE'),('Anna','PES','Ana','PES'),('Vinicius','MKT','Anna','MKT');
-/*!40000 ALTER TABLE `apadrinhamento` ENABLE KEYS */;
+-- Inserção de valores
+LOCK TABLES apadrinhamento WRITE;
+INSERT INTO apadrinhamento VALUES 
+  ('Joao', 'PRO', 'Mariana', 'PRO'),
+  ('Rodrigo', 'MKT', 'Anna', 'MKT'),
+  ('Bianca', 'PRE', 'Eduarda', 'PRE'),
+  ('Renan', 'PRE', 'Eduarda', 'PRE'),
+  ('Anna', 'PES', 'Ana', 'PES'),
+  ('Vinicius', 'MKT', 'Anna', 'MKT');
 UNLOCK TABLES;
 
---
--- Table structure for table `membros`
---
+-- Checagem de sucesso
+SELECT * FROM apadrinhamento;
 
-DROP TABLE IF EXISTS `membros`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `membros` (
-  `id_membros` int(11) NOT NULL,
-  `membro` text NOT NULL,
-  `idade` int(11) NOT NULL,
-  `genero` text NOT NULL,
-  `diretoria` text NOT NULL,
-  `projeto` text NOT NULL,
-  PRIMARY KEY (`id_membros`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS projetos;
+CREATE TABLE projetos (
+  id_projeto smallint AUTO_INCREMENT NOT NULL,
+  nome_projeto text NOT NULL,
+  quantidade_membros smallint NOT NULL,
+  project_owner text NOT NULL,
+  tempo_estimado_mes smallint NOT NULL,
+  PRIMARY KEY (id_projeto)
+);
 
---
--- Dumping data for table `membros`
---
-
-LOCK TABLES `membros` WRITE;
-/*!40000 ALTER TABLE `membros` DISABLE KEYS */;
-INSERT INTO `membros` VALUES (1,'Mariana',20,'F','PRO','MySQL'),(2,'Ana',19,'F','PES','C'),(3,'Eduarda',23,'F','PRE','Legado'),(4,'Rodrigo',21,'M','MKT','MySQL'),(5,'Joao',20,'M','PRO','MySQL'),(6,'Anna Gabriela',20,'F','MKT','C'),(7,'Lucas',21,'M','PES','C');
-/*!40000 ALTER TABLE `membros` ENABLE KEYS */;
+LOCK TABLES projetos WRITE;
+INSERT INTO projetos VALUES 
+  (1, 'C', 5, 'Matheus', 6),
+  (2, 'WebScraping', 3, 'Leandro', 5),
+  (3, 'OpenFOAM', 2, 'Carlos', 8),
+  (4, 'MachineLearning', 4, 'Emanuel', 10),
+  (5, 'MySQL', 3, 'Mariana', 7),
+  (6, 'Space Invaders', 2, 'Leandro', 6),
+  (7, 'POChE', 2, 'Mariana', 4);
 UNLOCK TABLES;
 
---
--- Table structure for table `pesquisa_satisfacao`
---
+DROP TABLE IF EXISTS membros;
+CREATE TABLE membros (
+  id_membros smallint AUTO_INCREMENT NOT NULL,
+  PRIMARY KEY (id_membros),
+  membro varchar(20) NOT NULL,
+  idade smallint NOT NULL,
+  genero text NOT NULL,
+  diretoria text NOT NULL,
+  id_projeto smallint NOT NULL,
+  FOREIGN KEY (id_projeto) REFERENCES projetos(id_projeto)
+);
 
-DROP TABLE IF EXISTS `pesquisa_satisfacao`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pesquisa_satisfacao` (
-  `ordem_preenchimento` int(11) NOT NULL,
-  `data` date DEFAULT NULL,
-  `diretoria` text NOT NULL,
-  `descricao` text NOT NULL,
-  PRIMARY KEY (`ordem_preenchimento`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `pesquisa_satisfacao`
---
-
-LOCK TABLES `pesquisa_satisfacao` WRITE;
-/*!40000 ALTER TABLE `pesquisa_satisfacao` DISABLE KEYS */;
-INSERT INTO `pesquisa_satisfacao` VALUES (1,'2024-08-12','PRO','Muito satisfeito'),(2,'2024-08-13','PES','Satisfeito'),(3,'2024-08-14','MKT','Muito satisfeito'),(4,'2024-08-12','PRO','Muito satisfeito'),(5,'2024-08-15','PRE','Pouco satisfeito'),(6,'2024-08-13','MKT','Satisfeito'),(7,'2024-08-12','PES','Muito satisfeito');
-/*!40000 ALTER TABLE `pesquisa_satisfacao` ENABLE KEYS */;
+LOCK TABLES membros WRITE;
+INSERT INTO membros VALUES 
+  (1, 'Mariana', 20, 'F', 'PRO', 5),
+  (2, 'Ana', 19, 'F', 'PES', 1),
+  (3, 'Eduarda', 23, 'F', 'PRE', 7),
+  (4, 'Rodrigo', 21, 'M', 'MKT', 5),
+  (5, 'Joao', 20, 'M', 'PRO', 5),
+  (6, 'Anna Gabriela', 20, 'F', 'MKT', 1),
+  (7, 'Lucas', 21, 'M', 'PES', 6),
+  (8, 'Carol', 22, 'F', 'PES', 3);
 UNLOCK TABLES;
 
---
--- Table structure for table `produtos`
---
+DROP TABLE IF EXISTS pesquisa_satisfacao;
+CREATE TABLE pesquisa_satisfacao (
+  ordem_preenchimento smallint AUTO_INCREMENT NOT NULL,
+  data date DEFAULT NULL,
+  diretoria text NOT NULL,
+  descricao text NOT NULL,
+  PRIMARY KEY (ordem_preenchimento)
+);
 
-DROP TABLE IF EXISTS `produtos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `produtos` (
-  `id_produto` int(11) NOT NULL,
-  `nome` text NOT NULL,
-  `fornecedor` text NOT NULL,
-  `id_membro_constante` int(11) NOT NULL,
-  `quantidade` int(11) NOT NULL,
-  `preco` int(11) NOT NULL,
-  PRIMARY KEY (`id_produto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `produtos`
---
-
-LOCK TABLES `produtos` WRITE;
-/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
-INSERT INTO `produtos` VALUES (1,'copos','grafica',1,50,15),(2,'camisas','grafica',2,30,40),(3,'chaveiro','grafica',3,20,20),(4,'adesivos','grafica',1,40,5);
-/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+LOCK TABLES pesquisa_satisfacao WRITE;
+INSERT INTO pesquisa_satisfacao VALUES
+  (1, '2024-08-12', 'PRO', 'Muito satisfeito'),
+  (2, '2024-08-13', 'PES', 'Satisfeito'),
+  (3, '2024-08-14', 'MKT', 'Muito satisfeito'),
+  (4, '2024-08-12', 'PRO', 'Muito satisfeito'),
+  (5, '2024-08-15', 'PRE', 'Pouco satisfeito'),
+  (6, '2024-08-13', 'MKT', 'Satisfeito'),
+  (7, '2024-08-12', 'PES', 'Muito satisfeito');
 UNLOCK TABLES;
 
---
--- Table structure for table `projetos`
---
+DROP TABLE IF EXISTS produtos;
+CREATE TABLE produtos (
+  id_produto smallint AUTO_INCREMENT NOT NULL,
+  nome text NOT NULL,
+  fornecedor text NOT NULL,
+  id_membro_contatante smallint NOT NULL,
+  FOREIGN KEY (id_membro_contatante) REFERENCES membros(id_membros), 
+  quantidade smallint NOT NULL,
+  preco smallint NOT NULL,
+  PRIMARY KEY (id_produto)
+);
 
-DROP TABLE IF EXISTS `projetos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `projetos` (
-  `id_projeto` int(11) NOT NULL,
-  `nome_projeto` text NOT NULL,
-  `quantidade_membros` int(11) NOT NULL,
-  `project_owner` text NOT NULL,
-  `tempo_estimado_mes` int(11) NOT NULL,
-  PRIMARY KEY (`id_projeto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `projetos`
---
-
-LOCK TABLES `projetos` WRITE;
-/*!40000 ALTER TABLE `projetos` DISABLE KEYS */;
-INSERT INTO `projetos` VALUES (1,'C',5,'Matheus',6),(2,'WebScraping',3,'Leandro',5),(3,'OpenFOAM',2,'Carlos',8),(4,'MachineLearning',4,'Emanuel',10),(5,'MySQL',3,'Mariana',7),(6,'Space Invaders',2,'Leandro',6),(7,'POChE',2,'Mariana',4);
-/*!40000 ALTER TABLE `projetos` ENABLE KEYS */;
+LOCK TABLES produtos WRITE;
+INSERT INTO produtos VALUES 
+  (1, 'copos', 'grafica', 1, 50, 15),
+  (2, 'camisas', 'grafica', 2, 30, 40),
+  (3, 'chaveiro', 'grafica', 3, 20, 20),
+  (4, 'adesivos', 'grafica', 1, 40, 5);
 UNLOCK TABLES;
 
---
--- Dumping events for database 'for_code'
---
+-- EXEMPLO UTILIZANDO GROUP BY
+SELECT diretoria, COUNT(*)
+ FROM membros
+	WHERE genero = 'F'
+	GROUP BY diretoria;
+    
+-- EXEMPLO 2 DE GROUP BY
+SELECT diretoria, COUNT(*) 
+FROM membros
+	WHERE genero = 'F' AND idade < 22
+	GROUP BY diretoria;
+    
+-- EXEMPLO DE JOIN
+SELECT membros.membro, membros.diretoria, membros.id_membros, produtos.nome, produtos.preco
+FROM membros
+INNER JOIN produtos
+	ON membros.id_membros = produtos.id_membro_contatante;
+   
+-- LEFT JOIN
+SELECT membros.membro, membros.diretoria, membros.id_membros, produtos.nome, produtos.preco
+FROM membros
+LEFT JOIN produtos
+ON membros.id_membros = produtos.id_membro_contatante;
 
---
--- Dumping routines for database 'for_code'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- RIGHT JOIN
+SELECT membros.membro, membros.diretoria, membros.id_membros, produtos.nome, produtos.preco
+FROM membros
+RIGHT JOIN produtos
+ON membros.id_membros = produtos.id_membro_contatante;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- CROSS JOIN
+SELECT membros.membro, membros.diretoria, membros.id_membros, produtos.nome, produtos.preco
+FROM membros
+CROSS JOIN produtos;
 
--- Dump completed on 2024-08-01 10:36:03
+-- Alguns bancos de dados, como o MySQL (até versão 5.7) não suportam o FULL JOIN. Como proceder?
+SELECT membros.membro, membros.diretoria, membros.id_membros, produtos.nome, produtos.preco
+FROM membros
+LEFT JOIN produtos
+ON membros.id_membros = produtos.id_membro_contatante
+UNION
+SELECT membros.membro, membros.diretoria, membros.id_membros, produtos.nome, produtos.preco
+FROM membros
+RIGHT JOIN produtos
+ON membros.id_membros = produtos.id_membro_contatante;
